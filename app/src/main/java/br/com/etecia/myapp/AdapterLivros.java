b63fb21d9825b2defd1e64cdb2d9a2a764bdc65c
Talita2687 <talitassf@gmail.com>
2026-05-28 22:14:01 -0300

    livros

--- app/src/main/java/br/com/etecia/myapp/AdapterLivros.java
+++ app/src/main/java/br/com/etecia/myapp/AdapterLivros.java
@@ -1,35 +1,57 @@
 package br.com.etecia.myapp;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.card.MaterialCardView;
 
+import java.util.List;
+
 public class AdapterLivros extends RecyclerView.Adapter<AdapterLivros.ViewHolder> {
+
+    public AdapterLivros(Context context, List<Livro> lstLivros) {
+        this.context = context;
+        this.lstLivros = lstLivros;
+    }
+
+    private Context context;
+    private List<Livro> lstLivros;
+
+
     @NonNull
     @Override
+
+
+
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        view = inflater.inflate(R.layout.modelo_livros, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.modeloTiutloLivros.setText(lstLivros.get(position).getTitulo());
+        holder.modeloImglivros.setImageResource(lstLivros.get(position).getImgLivro());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstLivros.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        MaterialCardView modeloCardLivros;
+        CardView modeloCardLivros;
         ImageView modeloImglivros;
         TextView modeloTiutloLivros;
         public ViewHolder(@NonNull View itemView) {
--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.etecia.myapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,8 +8,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Livro> lstLivros;
+    RecyclerView idRecLivros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        idRecLivros = findViewById(R.id.idRecLivros);
+
+        lstLivros = new ArrayList<>();
+
+        lstLivros.add(new Livro("Revolução dos Bichos", "Educativo", R.drawable.revolucaodosbichos, 29.3));
+        lstLivros.add(new Livro(" A Hora da Estrela", "Educativo", R.drawable.ahoradaestrela, 27.30));
+        lstLivros.add(new Livro("A metamorforse", "Educativo", R.drawable.meta, 20.3));
+        lstLivros.add(new Livro("Revolução dos Bichos", "Educativo", R.drawable.revolucaodosbichos, 29.3));
+        lstLivros.add(new Livro("Revolução dos Bichos", "Educativo", R.drawable.revolucaodosbichos, 29.3));
+        lstLivros.add(new Livro("Revolução dos Bichos", "Educativo", R.drawable.revolucaodosbichos, 29.3));
+        lstLivros.add(new Livro("Revolução dos Bichos", "Educativo", R.drawable.revolucaodosbichos, 29.3));
+        lstLivros.add(new Livro("Revolução dos Bichos", "Educativo", R.drawable.revolucaodosbichos, 29.3));
+        lstLivros.add(new Livro("Revolução dos Bichos", "Educativo", R.drawable.revolucaodosbichos, 29.3));
+        lstLivros.add(new Livro("Revolução dos Bichos", "Educativo", R.drawable.revolucaodosbichos, 29.3));
+
+        AdapterLivros adapterLivros = new AdapterLivros(getApplicationContext(), lstLivros);
+
+        idRecLivros.setAdapter(adapterLivros);
+
+        idRecLivros.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
     }
 }
